d1b7466b4a60e964546700c47c1a8ae9f92b2554
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-25 22:17:12 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-// console.log('EXECUTED!');
+import dotenv from 'dotenv';
+dotenv.config();
 
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-
-// const person: string = "Bob";
-// const count: number = 100;
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
