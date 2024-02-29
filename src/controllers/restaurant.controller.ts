34b0ed1c60cafd97fbe79d1c2b1c19ec414d49e0
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-29 15:29:31 +0900

    feat: member service model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,13 +1,14 @@
 // controllerlar har doim object lar orqali hosil qilinadi
 import { Request, Response } from 'express';
-import { T } from "../libs/types/common"
+import { T } from "../libs/types/common";
+import MemberService from "../models/Model.service";
 
 const restaurantContoller: T = {};
 //Bu yerda controllarga tegishli methodlarni qurib olamiz
 restaurantContoller.goHome = (req: Request, res: Response) => {
     try {
         res.send("Homepage");
-    } catch (err) {
+    } catch (err)  {
         console.log("Error, goHome", err);
     }
 
