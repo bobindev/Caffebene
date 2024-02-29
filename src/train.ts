8f779537b0d045008f35891c1820aa6245b1ebf0
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-02-29 17:16:55 +0900

    feat: task-H done

--- src/train.ts
+++ src/train.ts
@@ -4,5 +4,23 @@ H-TASK:
 shunday function tuzing, 
 u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
 MASALAN: getPositive([1, -4, 2]) return qiladi "12"
-*/
 
+Arithmetic:
+1. function yaratish
+2. o'zgaruvchi hosil qilish
+3. loop yaratish
+4. in condition: agar qiymat musbat bo'lsa, o'zgaruvchiga qiymat olish
+5. o'zgaruvchiga qiymat qo'shish
+6. qiymatni return qilish
+
+*/
+function getPositive(arr: any) {
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        result += arr[i];
+      }
+    } return result;
+  }
+  
+  console.log(getPositive([1, -4, 2])); 
