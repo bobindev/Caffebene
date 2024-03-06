66fb969ad2e4a54f7d0444b47da56f9882865d15
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-06 22:50:11 +0900

    feat: develop restaurant login process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from 'express';
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { MemberInput } from '../libs/types/member';
+import { LoginInput, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
 
 const restaurantContoller: T = {};
@@ -38,12 +38,19 @@ restaurantContoller.getSignup = (req: Request, res: Response) => {
 
 };
 
-restaurantContoller.processLogin = (req: Request, res: Response) => {
+restaurantContoller.processLogin = async(req: Request, res: Response) => {
     try {
         console.log("processLogin");
-        res.send("DONE");
+        console.log("body: ", req.body);
+        const input: LoginInput = req.body;
+
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input)
+        
+        res.send(result);
     } catch (err) {
         console.log("Error, processLogin", err);
+        res.send(err);
     }
 
 };
@@ -58,9 +65,10 @@ restaurantContoller.processSignup = async(req: Request, res: Response) => {
 
         const memberService = new MemberService();
         const result = await memberService.processSignup(newMember);
+
         res.send(result);
     } catch (err) {
-        console.log("Error, processSignup", err);
+        console.log("Error, processSignup:", err);
         res.send(err);
     }
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,35 +1,53 @@
 import MemberModel from "../schema/Member.model";
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 
 class MemberService {
     private readonly memberModel;
-    constructor () {
+    constructor() {
         this.memberModel = MemberModel;
     }
 
-    public async processSignup (input: MemberInput): Promise<Member> {
-       
-        
-        const exist = await this.memberModel.findOne({memberType: MemberType.RESTAURANT}).exec();
-       console.log("exist:", exist);
-       if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    public async processSignup(input: MemberInput): Promise<Member> {
+        const exist = await this.memberModel
+        .findOne({ memberType: MemberType.RESTAURANT })
+        .exec();
+        console.log("exist:", exist);
+        if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
 
         try {
             const result = await this.memberModel.create(input);
-        
-        // const tempResult = new this.memberModel(input);
-        // const result = await tempResult.save();
 
-        result.memberPassword = "";
+            // const tempResult = new this.memberModel(input);
+            // const result = await tempResult.save();
 
+            result.memberPassword = "";
+            return result;
 
-        return result;
-        } catch (err){
+        } catch (err) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED)
         }
-        
+
+    }
+
+    public async processLogin(input: LoginInput): Promise<Member> {
+        const member = await this.memberModel
+            .findOne({ memberNick: input.memberNick }, {memberNick: 1, memberPassword: 1})
+            .exec();
+
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = input.memberPassword === member.memberPassword;
+        console.log("isMatch:", isMatch);
+
+        if (!isMatch) {
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+        }
+
+        return await this.memberModel
+        .findById(member._id)
+        .exec();
     }
 }
 
