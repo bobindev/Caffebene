49e127fe19b2c0d8e139528fcd25c90050d9e368
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-14 07:20:11 +0900

    feat: configure product Schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,4 +1,10 @@
+import ProductModel from "../schema/Product.model";
 
-class ProductService{}
+class ProductService{
+    private readonly productModel;
+    constructor() {
+        this.productModel = ProductModel;
+    }
+}
 
 export default ProductService;
