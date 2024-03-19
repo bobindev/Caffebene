50212f8bb3b15ff52ccf92de1e18da379b6750d8
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-19 11:33:12 +0900

    feat: TASK-N done

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,13 @@
 console.log("TRAIN AREA");
- /*
-  M-TASK: 
+//TASK - N
 
-  Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni o'zi va hamda o'sha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bo'lgan objectlarni array ichida qaytarsin.
-  MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
-  */
+function palindromCheck(str:any) {
 
-  function getSquareNumbers(arr:any)  {
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-      result.push({number: arr[i], square: arr[i]**2});
-    }
-    return result;
+  const reversedString = str.split('').reverse().join('');
+  if(str === reversedString)
+        return true;
+  else
+        return false;
+}
 
-    
-  }
-console.log(getSquareNumbers([1, 2, 3]));
+console.log(palindromCheck ("kiyik"));
