321ea276c5fa51056898690b7fc894d06e78abfc
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-21 01:06:33 +0900

    feat: develop image pre-loading process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,30 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function() {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            console.log("uploadFile:", uploadFile)
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png",];
+            if (!validImageType.includes(fileType)) {
+                alert("Please insert only jpg, jpeg, and png!");
+            }else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".sign-input-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("success");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+});
 
 function validateSignupForm() {
     //console.log("EXECUTED validateSignupForm");
@@ -20,6 +44,13 @@ function validateSignupForm() {
 
         if (memberPassword !== confirmPassword) {
             alert("Password differs, please check!");
+            return false;
+        }
+
+        const membrImage = $(".member-image").get(0).files[0].name ? $(".member-image").get(0).files[0].name : null;
+
+        if(!membrImage) {
+            alert("Please insert restaurant image!");
             return false;
         }
     
