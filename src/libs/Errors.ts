30d9e19f2c2ce0fdec4b83ca96257a4fa072bdf5
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-23 03:38:12 +0900

    fix: modify SPA login api process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -17,6 +17,7 @@ export enum Message {
 
     USED_NICK_PHON = "You are inserting already used nick or phone!",
     NO_MEMBER_NICK = "No member with that member nickname!",
+    BLOCKED_USER = "You have been blocked, contact the restaurant!",
     WRONG_PASSWORD = "Wrong password, please try agoin!",
     NOT_AUTHENTICATED= "You are not authenticated, Please login first!"
 }
