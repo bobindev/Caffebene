0cd1f8ee73b4337d077c2ef8d2c9c1e28eb90146
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-03-23 12:34:13 +0900

    feat: TASK-P has been done

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,9 @@
 console.log("TRAIN AREA");
-//TASK - O
+//TASK - P
 
-function calculateSumOfNumbers(arr: any[]): any {
-      let sumeNumber = 0;
-      for (let i = 0; i < arr.length; i++) {
-          if (typeof arr[i] === "number"){
-            sumeNumber += arr[i];
-          }
-      }
-      return sumeNumber;
-  
-  }
-  
-  console.log(calculateSumOfNumbers([10, "10", {son: 10}, true, 35]));
+function objectToArray(obj:any) {
+      let result = Object.entries(obj);
+       return result;
+   }
+   console.log(objectToArray( {a: 10, b: 20}));
+
