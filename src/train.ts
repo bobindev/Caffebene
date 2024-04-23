fa1d132497cfb9b2675e1c2cca5fc5e2f1f61940
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-04-23 09:54:55 +0900

    feat: Task-ZB and Task_ZC are done

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,17 @@
 console.log("TRAIN AREA");
-//TASK - ZA
+//TASK - ZB
 
-function sortByAge(arr: any) {
-  return arr.sort((a: any, b: any) => a.age - b.age);
+function randomBetween (min:number, max:number) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-console.log(sortByAge([{ age: 23 }, { age: 21 }, { age: 13 }]));
+console.log(randomBetween(30,50));
+
+//TASK - ZC
+
+function celsiusToFahrenheit(cel:number) {
+  let fahren = (cel * 9/5) + 32;
+  return fahren;
+}
+
+console.log(celsiusToFahrenheit(0));
